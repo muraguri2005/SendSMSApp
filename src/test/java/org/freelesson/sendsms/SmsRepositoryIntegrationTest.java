f9ead38b6c3fd336fc89dec2ff71a6a515317a1c
Richard Gathogo <richard@tulaa.io>
2019-06-18 16:21:04 +0300

    remove unnecessary imports

--- src/test/java/org/freelesson/sendsms/SmsRepositoryIntegrationTest.java
+++ src/test/java/org/freelesson/sendsms/SmsRepositoryIntegrationTest.java
@@ -1,13 +1,6 @@
 package org.freelesson.sendsms;
 
-import static org.junit.Assert.assertEquals;
-
-import java.util.Date;
-
-import org.freelesson.sendsms.domain.Sms;
-import org.freelesson.sendsms.domain.enums.SmsStatus;
 import org.freelesson.sendsms.repository.SmsRepository;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
