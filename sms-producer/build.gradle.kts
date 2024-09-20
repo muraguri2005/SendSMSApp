4ae5a5d10df7642d74d29efcb4587dd50a5c52c3
Richard Gathogo <rmgathogo@kcbgroup.com>
2024-09-20 13:49:42 +0300

    general improvements

--- sms-producer/build.gradle.kts
+++ sms-producer/build.gradle.kts
@@ -18,6 +18,7 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework.kafka:spring-kafka")
