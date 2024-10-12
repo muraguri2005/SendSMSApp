c3f02df92753a27a58c65dbbd97951593c4154ea
Richard Muraguri <muraguri2005@gmail.com>
2024-10-12 21:36:44 +0300

    connect to kafka running on docker compose

--- sms-consumer/src/main/java/com/datatab/service/SendSMSService.java
+++ sms-consumer/src/main/java/com/datatab/service/SendSMSService.java
@@ -4,15 +4,15 @@ import com.africastalking.AfricasTalking;
 import com.africastalking.sms.Recipient;
 import com.datatab.domain.Sms;
 import com.datatab.domain.enums.SmsStatus;
-import com.datatab.exception.BaseException;
 import com.datatab.properties.SmsConsumerProperties;
 import com.datatab.repository.SmsRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -20,44 +20,51 @@ public class SendSMSService {
     final SmsService smsService;
     final SmsRepository smsRepository;
     private final Logger log = LoggerFactory.getLogger(this.getClass());
+    private final ObjectMapper objectMapper;
 
-    public SendSMSService(SmsService smsService, SmsRepository smsRepository, SmsConsumerProperties smsConsumerProperties) {
+    public SendSMSService(SmsService smsService, SmsRepository smsRepository, SmsConsumerProperties smsConsumerProperties, @Qualifier("objectMapper") ObjectMapper objectMapper) {
         AfricasTalking.initialize(smsConsumerProperties.africasTalkingUsername(), smsConsumerProperties.africasTalkingPassword());
         this.smsService = smsService;
         this.smsRepository = smsRepository;
+        this.objectMapper = objectMapper;
     }
 
     @KafkaListener(topics = "${com.datatab.send-sms-queue-name}", groupId = "${com.datatab.send-sms-queue-group-id}")
-    public void sendSMSFromQueue(Sms sms) {
-        com.africastalking.SmsService atSmsService = AfricasTalking.getService(AfricasTalking.SERVICE_SMS);
+    public void sendSMSFromQueue(String smsString) {
+        Sms sms = null;
         try {
+            sms = objectMapper.readValue(smsString, Sms.class);
+            com.africastalking.SmsService atSmsService = AfricasTalking.getService(AfricasTalking.SERVICE_SMS);
             log.info("sending sms via africastalking");
-            List<Recipient> response = atSmsService.send(sms.message, "RMG", new String[]{sms.recepient}, true);
+            List<Recipient> response = atSmsService.send(sms.message, "RMG", new String[]{sms.recipient}, true);
             sms.status = SmsStatus.SENT;
             log.info("sending sms response {}", response);
             if (!response.isEmpty()) {
                 Recipient recipient = response.getFirst();
                 sms.externalId = recipient.messageId;
-                try {
-                    sms.cost = Double.parseDouble(recipient.cost.split(" ")[1]);
-                } catch (Exception e) {
-                    log.error("error getting cost {}", e.getMessage());
-                }
                 if (!recipient.status.equals("Success")) {
                     sms.status = SmsStatus.FAILED;
                     sms.statusComments = recipient.status;
+                } else {
+                    sms.status = SmsStatus.SENT;
                 }
+                sms.cost = Double.parseDouble(recipient.cost.split(" ")[1]);
             }
-        } catch (IOException e) {
-            sms.status = SmsStatus.FAILED;
-            sms.statusComments = e.getMessage();
 
-            log.error("error sending sms", e);
+
+        } catch (Exception e) {
+            log.error("error processing sms", e);
+            if (sms != null) {
+                sms.status = SmsStatus.FAILED;
+                sms.statusComments = e.getMessage();
+            }
         }
+
+
         try {
             smsService.update(sms);
-        } catch (BaseException e) {
-            log.error("error updating sms", e);
+        } catch (Exception e) {
+            log.error("error saving ");
         }
 
     }
